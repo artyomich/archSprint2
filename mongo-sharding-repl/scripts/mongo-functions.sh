17c40ddeef6ad1c5ec2d693e2b174abccb176793
artem <arifulin@gmail.com>
2025-05-06 18:45:31 -0400

    #2 mongo-shardings-repl

--- mongo-sharding-repl/scripts/mongo-functions.sh
+++ mongo-sharding-repl/scripts/mongo-functions.sh
@@ -27,15 +27,14 @@ echo "All shards initializing..."
 
 echo "try shard1..."
 
-docker compose exec -T shard1 mongosh --port 27018 --quiet << EOF
-  rs.initiate(
-      {
-        _id : "shard1",
-        members: [
-          { _id : 0, host : "shard1:27018" }
-        ]
-      }
-  );
+docker compose exec -T shard1_node1 mongosh --port 27018 --quiet << EOF
+  rs.initiate({_id: "shard1_node1", members:
+    [
+      {_id: 0, host: "shard1_node1:27018"},
+      {_id: 1, host: "shard1_node2:27019"},
+      {_id: 2, host: "shard1_node3:27020"}
+    ]
+  });
   exit();
 EOF
 
@@ -43,15 +42,14 @@ echo "shard1 is initialized...."
 
 echo "try shard2..."
 
-docker compose exec -T shard2 mongosh --port 27019 --quiet << EOF
-  rs.initiate(
-      {
-        _id : "shard2",
-        members: [
-          { _id : 1, host : "shard2:27019" }
-        ]
-      }
-  );
+docker compose exec -T shard2_node1 mongosh --port 27021 --quiet << EOF
+  rs.initiate({_id: "shard2_node1", members:
+    [
+      {_id: 0, host: "shard2_node1:27021"},
+      {_id: 1, host: "shard2_node2:27022"},
+      {_id: 2, host: "shard2_node3:27023"}
+    ]
+  });
   exit();
 EOF
 echo "shard2 is initialized..."
@@ -60,7 +58,7 @@ echo "shard2 is initialized..."
 waitingForRouter()
 {
   local host="mongos_router"
-  local port="27020"
+  local port="27024"
   local maxAttempts=20
   local attempt=0
 
@@ -85,9 +83,9 @@ initRouter()
 waitingForRouter
 
 echo "Turn on mongos_router..."
-docker compose exec -T mongos_router mongosh --port 27020 --quiet << EOF
-  sh.addShard( "shard1/shard1:27018");
-  sh.addShard( "shard2/shard2:27019");
+docker compose exec -T mongos_router mongosh --port 27024 --quiet << EOF
+  sh.addShard( "shard1_node1/shard1_node1:27018");
+  sh.addShard( "shard2_node1/shard2_node1:27021");
   sh.enableSharding("somedb");
   sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
   use somedb
--- mongo-sharding/scripts/mongo-init.sh
+++ mongo-sharding/scripts/mongo-init.sh
@@ -6,3 +6,6 @@ source "${CURRENT_DIR}/mongo-functions.sh"
 initConfigSrv  # Инициализация configSrv
 initShards     # Инициализация шарды
 initRouter     # Инициализация mongo роутера + сидирование
+
+getDocsCount shard1 27018  # Записи в shard1
+getDocsCount shard2 27019  # Записи в shard2
